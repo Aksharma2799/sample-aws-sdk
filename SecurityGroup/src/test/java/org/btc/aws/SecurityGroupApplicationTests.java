18b09d52f952587a5c4269cd7855f1e39db17c9f
ASHISH VISHWAKARMA <75712767+Aksharma2799@users.noreply.github.com>
2022-12-28 16:13:46 +0530

    Add files via upload
    AWS Security group using Java Sdk v2

---
+++ SecurityGroup/src/test/java/org/btc/aws/SecurityGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package org.btc.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityGroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
